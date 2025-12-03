cf8aa36be72da7ad3f0921ea7e495274af657444
JayAcc2025 <jay.f.capucion@accenture.com>
2025-12-03 04:20:11 +0000

    added database structure

---
+++ octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
@@ -0,0 +1,92 @@
+from django.core.management.base import BaseCommand
+from django.conf import settings
+from djongo import models
+
+# Define models for each collection
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    team = models.CharField(max_length=50)
+    class Meta:
+        app_label = 'octofit_tracker'
+        db_table = 'users'
+
+class Team(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    class Meta:
+        app_label = 'octofit_tracker'
+        db_table = 'teams'
+
+class Activity(models.Model):
+    user_email = models.EmailField()
+    type = models.CharField(max_length=50)
+    duration = models.IntegerField()
+    class Meta:
+        app_label = 'octofit_tracker'
+        db_table = 'activities'
+
+class Leaderboard(models.Model):
+    team = models.CharField(max_length=50)
+    points = models.IntegerField()
+    class Meta:
+        app_label = 'octofit_tracker'
+        db_table = 'leaderboard'
+
+class Workout(models.Model):
+    name = models.CharField(max_length=100)
+    difficulty = models.CharField(max_length=50)
+    class Meta:
+        app_label = 'octofit_tracker'
+        db_table = 'workouts'
+
+class Command(BaseCommand):
+    help = 'Populate the octofit_db database with test data'
+
+    def handle(self, *args, **options):
+        # Delete all data
+        User.objects.all().delete()
+        Team.objects.all().delete()
+        Activity.objects.all().delete()
+        Leaderboard.objects.all().delete()
+        Workout.objects.all().delete()
+
+        # Ensure unique index on email field for users collection
+        from pymongo import MongoClient
+        client = MongoClient('localhost', 27017)
+        db = client['octofit_db']
+        db.users.create_index([('email', 1)], unique=True)
+
+        # Create teams
+        marvel = Team.objects.create(name='Marvel')
+        dc = Team.objects.create(name='DC')
+
+        # Create users (super heroes)
+        users = [
+            User(name='Spider-Man', email='spiderman@marvel.com', team='Marvel'),
+            User(name='Iron Man', email='ironman@marvel.com', team='Marvel'),
+            User(name='Wonder Woman', email='wonderwoman@dc.com', team='DC'),
+            User(name='Batman', email='batman@dc.com', team='DC'),
+        ]
+        User.objects.bulk_create(users)
+
+        # Create activities
+        activities = [
+            Activity(user_email='spiderman@marvel.com', type='Running', duration=30),
+            Activity(user_email='ironman@marvel.com', type='Cycling', duration=45),
+            Activity(user_email='wonderwoman@dc.com', type='Swimming', duration=60),
+            Activity(user_email='batman@dc.com', type='Yoga', duration=20),
+        ]
+        Activity.objects.bulk_create(activities)
+
+        # Create leaderboard
+        Leaderboard.objects.create(team='Marvel', points=75)
+        Leaderboard.objects.create(team='DC', points=80)
+
+        # Create workouts
+        workouts = [
+            Workout(name='Hero HIIT', difficulty='Hard'),
+            Workout(name='Power Yoga', difficulty='Medium'),
+        ]
+        Workout.objects.bulk_create(workouts)
+
+        self.stdout.write(self.style.SUCCESS('octofit_db database populated with test data.'))
